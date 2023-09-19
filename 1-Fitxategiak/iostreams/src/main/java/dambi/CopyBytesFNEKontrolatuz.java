5b4e2bb2b7eb7371852e98c3471a83eed2440a4b
julengalin <galindo.julen@uni.eus>
2023-09-19 13:59:04 +0200

    M

--- 1-Fitxategiak/iostreams/src/main/java/dambi/CopyBytesFNEKontrolatuz.java
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyBytesFNEKontrolatuz.java
@@ -4,29 +4,32 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Scanner;
 
-/** CopyBytes baina mezu aproposa ateraz kopiatu behar duen fitxategia aurkitzen ez badu. 
- * Horretarako, FileNotFoundException salbuespena harrapatu beharko duzu. 
- * (Ikus SalbuespeneiBuruzkoOinarrizkoAzalpenak edo bertan aipatzen diren iturriak)    
-    */
-    public class CopyBytesFNEKontrolatuz {
-     public static void main(String[] args) throws IOException {
+public class CopyBytesFNEKontrolatuz {
+
+    /* Programa honek, FileNotFound exception-a ateratzean, errore mezu bat bueltatuko du.*/
+    public static void main(String[] args) throws IOException {
 
         FileInputStream in = null;
         FileOutputStream out = null;
 
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sartu kopiatzeko fitxategiaren izena (outagain.txt fitxategian kopiatuko da): ");
+        String input_fitxategia = input.next();
+        input.close();
+        
         try {
-            in = new FileInputStream("xanadu.txt");
+            in = new FileInputStream(input_fitxategia);
             out = new FileOutputStream("outagain.txt");
             int c;
 
             while ((c = in.read()) != -1) {
                 out.write(c);
             }
-        } catch(FileNotFoundException e){
-            System.out.print("Ez da aurkitu fitxategia");
-        }
-        finally {
+        } catch (FileNotFoundException e){
+            System.out.println("Fitxategia ez da aurkitu.");
+        } finally {
             if (in != null) {
                 in.close();
             }
@@ -36,4 +39,3 @@ import java.io.IOException;
         }
     }
 }
-
--- 1-Fitxategiak/iostreams/src/main/java/dambi/CopyCharactersMaiuskulaz.java
+++ 1-Fitxategiak/iostreams/src/main/java/dambi/CopyCharactersMaiuskulaz.java
@@ -1,5 +1,36 @@
 package dambi;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class CopyCharactersMaiuskulaz {
-    
+    public static void main(String[] args) throws IOException {
+
+        /* Programa honek, karaktere denak maiuskulaz jartzen ditu. */
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("xanadu.txt");
+            outputStream = new FileWriter("characteroutput.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                
+                if(c >= 97 && c <= 122 ){
+                    c -= 32;
+                }
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
 }
